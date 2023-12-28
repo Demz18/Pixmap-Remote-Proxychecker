c2d17b7bbe9d36c8372aba4f8bfb7718d2ef292f
Demz18 <111712178+Demz18@users.noreply.github.com>
2023-12-28 13:20:43 -0300

    Added error codes meaning

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-import requests
+try:
+    import requests
+except ImportError:
+    subprocess.check_call(['pip', 'install', 'requests'])
+try:
+    import colorama
+except ImportError:
+    subprocess.check_call(['pip', 'install', 'colorama'])
 
+#inicia colorama
+colorama.init()
 
+GREEN = colorama.Fore.GREEN
+RESET = colorama.Fore.RESET
+def print_green(msg):
+    print(f"{GREEN}{msg}{RESET}")
+
+print_green ("       /$$$$$$$ /$$$$$$$  /$$$$$$ /$$   /$$/$$     /$$          ")
+print_green ("      | $$__  $| $$__  $$/$$__  $| $$  / $|  $$   /$$/          ")
+print_green ("      | $$__  $| $$__  $$/$$__  $| $$  / $|  $$   /$$/          ")
+print_green ("      | $$  \ $| $$  \ $| $$  \ $|  $$/ $$/\  $$ /$$/           ")
+print_green ("      | $$$$$$$| $$$$$$$| $$  | $$\  $$$$/  \  $$$$/            ")
+print_green ("      | $$____/| $$__  $| $$  | $$ >$$  $$   \  $$/             ")
+print_green ("      | $$     | $$  \ $| $$  | $$/$$/\  $$   | $$              ")
+print_green ("      | $$     | $$  | $|  $$$$$$| $$  \ $$   | $$              ")
+print_green ("      |__/     |__/  |__/\______/|__/  |__/   |__/              ")
+print_green ("  /$$$$$$ /$$   /$$/$$$$$$$$ /$$$$$$ /$$   /$$/$$$$$$$$/$$$$$$$ ")
+print_green (" /$$__  $| $$  | $| $$_____//$$__  $| $$  /$$| $$_____| $$__  $$")
+print_green ("| $$  \__| $$  | $| $$     | $$  \__| $$ /$$/| $$     | $$  \ $$")
+print_green ("| $$     | $$$$$$$| $$$$$  | $$     | $$$$$/ | $$$$$  | $$$$$$$/")
+print_green ("| $$     | $$__  $| $$__/  | $$     | $$  $$ | $$__/  | $$__  $$")
+print_green ("| $$    $| $$  | $| $$     | $$    $| $$\  $$| $$     | $$  \ $$")
+print_green ("|  $$$$$$| $$  | $| $$$$$$$|  $$$$$$| $$ \  $| $$$$$$$| $$  | $$")
+print_green (" \______/|__/  |__|________/\______/|__/  \__|________|__/  |__/")
+print_green ("                                                                ")
+print_green ("  _                 _                    ")
+print_green (" | |               | |                   ")
+print_green (" | |__  _   _    __| | ___ _ __ ___  ____")
+print_green (" | '_ \| | | |  / _` |/ _ | '_ ` _ \|_  /")
+print_green (" | |_) | |_| | | (_| |  __| | | | | |/ / ")
+print_green (" |_.__/ \__, |  \__,_|\___|_| |_| |_/___|")
+print_green ("         __/ |                           ")
+print_green ("        |___/                            ")
+print_green ("                                                                ")
+print_green ("                                                                ")
 
 try:
     while True:
@@ -10,9 +52,9 @@ try:
             with open('api.txt', 'r') as api:
                 apikey = api.read()
             # Pide la IP por consola
-            ip = input("IP: ")
+            ip = input("\nIP: ")
 
-            # URL de la API que deseas llamar
+            # URL de la API
             url = 'https://api.whatismyip.com/proxy.php'
 
             # Agrega la IP para chequear
@@ -25,13 +67,17 @@ try:
             if response.status_code == 200:
                 # La respuesta de la API se encuentra en response.text o response.json() si es JSON
                 data = response.json()  # Si la respuesta es JSON
-                print(data)
+                if data == 1 :
+                    print ("Not a valid API key")
+                    print (f'Error code: {data}')
+                elif data == 3 :
+                    print ("Too many lookups today")
+                    print (f'Error code: {data}')
+                else :
+                    print (data)
             else:
                 print(f'Error al hacer la solicitud: Codigo de estado {response.status_code}')
 
-        #    continuar = input("Do you want to continue checking ips? (Yes/No): ")
-        #    if continuar == 'no' or 'n':
-        #       break  # Sale del bucle si la respuesta no es "si" (puedes personalizar según tus necesidades)
     pass
 except KeyboardInterrupt:
     print("\n\nGoodbye")
